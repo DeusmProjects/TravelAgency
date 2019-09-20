8f60d931742cbc73a7e3b28ac9b1965c24ba90c4
deusmemes <maxin29999@gmail.com>
2019-09-20 12:54:36 +0400

    port heroku

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname + '/dist/TravelAgency/index.html'));
 });
 
-// app.listen(process.env.PORT || 8080);
-app.listen(3001);
+app.listen(process.env.PORT || 8080);
+// app.listen(3001);
