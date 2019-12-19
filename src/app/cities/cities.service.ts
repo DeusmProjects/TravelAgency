56d11d33e0912f62c5ea3dee451eee4a3870ac69
deusmemes <maxin29999@gmail.com>
2019-12-19 23:34:37 +0400

    heroku test

--- src/app/cities/cities.service.ts
+++ src/app/cities/cities.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {City} from '../models/city';
-import {environment} from "../../environments/environment";
+import {environment} from "../../environments/environment.prod";
 
 @Injectable()
 export class CitiesService {
--- src/app/hotels/hotels.service.ts
+++ src/app/hotels/hotels.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable, of} from 'rxjs';
 import {Hotel} from '../models/hotel';
-import { environment } from "../../environments/environment";
+import { environment } from "../../environments/environment.prod";
 
 @Injectable()
 export class HotelsService {
